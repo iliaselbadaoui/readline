6ed7ee4b8eefb3cd1f4abf43b0fd9c297462c87a
Ilias Elbadaoui <iliassovich@outlook.com>
2021-03-27 22:29:41 +0100

    readline from home

--- newline.c
+++ newline.c
@@ -1,51 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   readline.c                                         :+:      :+:    :+:   */
+/*   newline.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ielbadao <ielbadao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/27 14:13:49 by ielbadao          #+#    #+#             */
-/*   Updated: 2021/03/27 22:04:50 by ielbadao         ###   ########.fr       */
+/*   Created: 2021/03/27 22:08:52 by ielbadao          #+#    #+#             */
+/*   Updated: 2021/03/27 22:09:24 by ielbadao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "readline.h"
-
-static void	concat_line(char **line, char *c)
-{
-	char	*tmp;
-
-	tmp = *line;
-	*line = ft_strjoin(*line, c);
-	free(tmp);
-}
 
 static void	minishell()
 {
 	write(1, "minishell$ ", 11);
 }
 
-char		*readline()
+void		newline()
 {
-	int				done;
-	int				total;
-	char			*res;
-
-	done = 1;
-	res = ft_strdup("");
-	while (done)
-	{
-		total = ft_getchar();
-		if (total >= 32 && total < 127)
-		{
-			write(1, &total, sizeof(int));
-			concat_line(&res, (char *)&total);
-		}
-		if (total == KEY_UP)
-			printf("\nUP\n");
-		if (total == ENTER)
-			done = 0;
-	}
-	return (res);
+	
 }
